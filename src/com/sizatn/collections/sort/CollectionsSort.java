c9b91b1bbaa883f6145b413536818b79bd07c441
sizatn <sizatn2013@gmail.com>
2018-01-28 11:55:57 +0800

    update

--- src/com/sizatn/collections/sort/CollectionsSort.java
+++ src/com/sizatn/collections/sort/CollectionsSort.java
@@ -1,8 +1,6 @@
 package com.sizatn.collections.sort;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class CollectionsSort {
